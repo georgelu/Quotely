bdcaaaf6f7ac43bd27f5419d32e6628c60392c7d
George Lu <georgelu@stanford.edu>
2012-12-01 19:36:26 -0800

    updated css and html

--- header.php
+++ header.php
@@ -1,23 +1,23 @@
-<div data-role="popup" id="popupAccordion1" data-corners="false" data-transition="slideup" data-theme="c" style="width:300px;">
-					<a href="quote.php?category=news&id=1" data-role="button" data-theme="c" data-corners="false">News</a>
-					<a href="quote.php?subcat=politics&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Politics</a>
-					<a href="quote.php?subcat=international&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;International</a>
-					<a href="quote.php?subcat=business&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Business</a>
-					<a href="quote.php?subcat=sports&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Sports</a>
+<div data-role="popup" id="popupAccordion1" data-corners="false" data-transition="slideup" data-theme="a" style="width:300px;">
+					<a href="quote.php?category=news&id=1" data-role="button" data-theme="a" data-corners="false">News</a>
+					<a href="quote.php?subcat=politics&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Politics</a>
+					<a href="quote.php?subcat=international&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;International</a>
+					<a href="quote.php?subcat=business&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Business</a>
+					<a href="quote.php?subcat=sports&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Sports</a>
 		</div>
 		
-		<div data-role="popup" id="popupAccordion2"  data-corners="false" data-transition="slideup" data-theme="c" style="width:300px;">
-					<a href="quote.php?category=art&id=1" data-role="button" data-theme="c" data-corners="false">Arts</a>
-					<a href="quote.php?subcat=music&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Music</a>
-					<a href="quote.php?subcat=literature&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Literature</a>
-					<a href="quote.php?subcat=visual&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Visual</a>
+		<div data-role="popup" id="popupAccordion2"  data-corners="false" data-transition="slideup" data-theme="a" style="width:300px;">
+					<a href="quote.php?category=art&id=1" data-role="button" data-theme="a" data-corners="false">Arts</a>
+					<a href="quote.php?subcat=music&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Music</a>
+					<a href="quote.php?subcat=literature&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Literature</a>
+					<a href="quote.php?subcat=visual&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Visual</a>
 		</div>
 		
-		<div data-role="popup" id="popupAccordion3" data-corners="false" data-transition="slideup" data-theme="c" style="width:300px;">
-					<a href="quote.php?category=inspiration&id=1" data-role="button" data-theme="c" data-corners="false">Thoughts</a>
-					<a href="quote.php?subcat=philosophical&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Philosophical</a>
-					<a href="quote.php?subcat=motivational&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Motivational</a>
-					<a href="quote.php?subcat=humor&subid=1" data-role="button" data-theme="c" data-corners="false">&nbsp;&nbsp;Humor</a>
+		<div data-role="popup" id="popupAccordion3" data-corners="false" data-transition="slideup" data-theme="a" style="width:300px;">
+					<a href="quote.php?category=inspiration&id=1" data-role="button" data-theme="a" data-corners="false">Thoughts</a>
+					<a href="quote.php?subcat=philosophical&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Philosophical</a>
+					<a href="quote.php?subcat=motivational&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Motivational</a>
+					<a href="quote.php?subcat=humor&subid=1" data-role="button" data-theme="a" data-corners="false">&nbsp;&nbsp;Humor</a>
 		</div>
 		
 		<div data-role="navbar">
